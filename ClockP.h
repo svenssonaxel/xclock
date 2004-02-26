61f5ea4644b373e3f8cc7c71d9f4d658707eff9a
Egbert Eich <eich@suse.de>
2004-02-26 13:36:16 +0000

    readding XFree86's cvs IDs

--- ClockP.h
+++ ClockP.h
@@ -49,7 +49,7 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
 ******************************************************************/
-/* $XFree86$ */
+/* $XFree86: xc/programs/xclock/ClockP.h,v 1.12 2002/10/21 13:33:08 alanh Exp $ */
 
 #ifndef _XawClockP_h
 #define _XawClockP_h
