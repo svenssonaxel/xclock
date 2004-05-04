ae716208894aa6b93da3b4d71420dba2edf50550
Eamon Walsh <ewalsh@epoch.ncsc.mil>
2004-05-04 19:44:05 +0000

    Merge the new release from HEAD

--- Clock.c
+++ Clock.c
@@ -1,5 +1,5 @@
 /* $Xorg: Clock.c,v 1.4 2001/02/09 02:05:39 xorgcvs Exp $ */
-/* $XdotOrg$ */
+/* $XdotOrg: xc/programs/xclock/Clock.c,v 1.2 2004/04/23 19:54:39 eich Exp $ */
 
 /***********************************************************
 
